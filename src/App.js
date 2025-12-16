9f3363853180e834336e91129bcf368c66e91485
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-16 14:54:56 +0530

    router set-up

--- src/App.js
+++ src/App.js
@@ -1,52 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Routes, Route, useParams } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
+import NewsList from './NewsList';
 
 function App() {
-  const [list , setList] = useState([])
-  const fetchdata = async()=>{
-    const data = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2025-11-16&sortBy=publishedAt&apiKey=be86f4663b6a4df1905eefd41ba43afd")
-    const res = await data.json()
-    setList(res.articles)
-    console.log(res.articles)
-  }
-  useEffect(()=>{
-    fetchdata()
-  },[])
-return (
-  <div className="App container mt-4 mb-8">
-    <Navbar/>
-    <div className="row g-4 mt-5">
-      {list.map((val, idx) => (
-        <div className="col-12 col-sm-6 col-md-4 col-lg-4 mt-16" key={idx}>
-          <div className="card h-100">
-            <img
-              src={val.urlToImage || "https://library.ceu.edu/wp-content/uploads/news-2444778_960_720.jpg"}
-              className="card-img-top"
-              alt="news"
-              style={{ height: "200px", objectFit: "cover" }}
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">{val.title}</h5>
-              <p className="card-text">{val.content}</p>
-              <a
-                href={val.url}
-                target="_blank"
-                rel="noreferrer"
-                className="btn btn-primary mt-auto"
-              >
-                See More...
-              </a>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  </div>
-);
-
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<NewsList />} />
+        <Route path="/:category" element={<NewsList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/NewsList.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+
+const NewsList = () => {
+  const { category } = useParams(); // 👈 from URL
+  const [list, setList] = useState([]);
+
+  const fetchdata = async () => {
+    const data = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=be86f4663b6a4df1905eefd41ba43afd`
+    );
+    const res = await data.json();
+    setList(res.articles || []);
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, [category]); // 🔥 refetch on URL change
+
+  return (
+    <div className="container mt-5">
+      <div className="row g-4">
+        {list.map((val, idx) => (
+          <div className="col-12 col-sm-6 col-md-4" key={idx}>
+            <div className="card h-100">
+              <img
+                src={val.urlToImage || "https://library.ceu.edu/wp-content/uploads/news-2444778_960_720.jpg"}
+                className="card-img-top"
+                alt="news"
+                style={{ height: "200px", objectFit: "cover" }}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{val.title}</h5>
+                <p className="card-text">{val.content}</p>
+                <a
+                  href={val.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="btn btn-primary mt-auto"
+                >
+                  See More...
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default NewsList
