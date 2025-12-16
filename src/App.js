c46805788e13401eebc851de92c7308ed1ed0763
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-16 13:49:28 +0530

    Initial commit - News app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [list , setList] = useState([])
+  const fetchdata = async()=>{
+    const data = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2025-11-16&sortBy=publishedAt&apiKey=be86f4663b6a4df1905eefd41ba43afd")
+    const res = await data.json()
+    setList(res.articles)
+    console.log(res.articles)
+  }
+  useEffect(()=>{
+    fetchdata()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='grid'>
+      {list .map((val, idx)=>(
+        // <div>{val.urlToImage}</div>
+        <div className="card" style={{ width: "18rem" }}>
+        <img src={val?.urlToImage} className="card-img-top" height={"200px"} width={'350px'} alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{val.title}</h5>
+          <p className="card-text">{val.content}</p>
+          <a href={val.url} className="btn btn-primary">See More...</a>
+        </div>
+      </div>
+      ))} 
+      </div>
+     
     </div>
   );
 }
